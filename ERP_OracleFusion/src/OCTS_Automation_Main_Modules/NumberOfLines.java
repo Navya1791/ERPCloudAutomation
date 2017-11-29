a0fd64a8b0f0beeda8dcc3b542c530e70bedf1d7
Navya Mallajosyula <33718383+Navya1791@users.noreply.github.com>
2017-11-29 16:43:32 +0530

    Add files via upload

--- ERP_OracleFusion/src/OCTS_Automation_Main_Modules/NumberOfLines.java
+++ ERP_OracleFusion/src/OCTS_Automation_Main_Modules/NumberOfLines.java
@@ -2,7 +2,7 @@
         CLASS Name:    NumberOfLines
         CREATED BY:    Navya Mallajosyula
         DATE CREATED:  Nov 2017
-        DESCRIPTION:   Validation of Number of Lines from GI interface file                    
+        DESCRIPTION:   Number of Lines  Validation                    
         PARAMETERS:                                                                  
         RETURNS:      
         COMMENTS:                                     
@@ -18,16 +18,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-
+import com.aventstack.extentreports.Status;
+import OCTS_Automation_Main_Modules.ReadERPInputDataSheet;
+import Common_Utility.ReporterBaseTest;
 import org.testng.annotations.Test;
 
-public class NumberOfLines {
+public class NumberOfLines extends ReporterBaseTest{
 
 	// Required Variables to pull out the matching string
 
-		/*static String zipFilePath = "C:\\Automation_OCTS\\Output\\70594.zip";  
-		static String destDir = "C:\\Automation_OCTS\\Output\\";*/
-		static String fileObj = ERP_Financial_Webservice_MainClass.zipFilePath.replace("zip", "txt");
+	//	static String zipFilePath = "C:\\Automation_OCTS\\Output\\71963.zip";  
+		static String fileObj;
+		static String destDir = "C:\\Automation_OCTS\\Output\\";
+	//	static String fileObj = zipFilePath.replace("zip", "txt");
 		static int numberOfLines;
 		static String[] splitNewLine = {};
 		static String finalOutputString = "";
@@ -35,7 +38,12 @@ public class NumberOfLines {
 		int flag=0;
 		@Test
 		public void numberOfLines() throws IOException {
-			
+			fileObj=ERP_Financial_Webservice_MainClass.zipFilePath.replace("zip", "txt");
+			System.out.println(fileObj);
+			 test=extent.createTest("Number of Lines");
+			//unzip the output file to enable comparision
+		//	UnzipOutputFileGenerated unZip = new UnzipOutputFileGenerated();
+			//unZip.unzip(zipFilePath, destDir);
 			//Get Number of Lines from GlInterface file
 			ReadERPInputDataSheet inputGIInterface = new ReadERPInputDataSheet();
 			numberOfLines = inputGIInterface.parseGIInterfaceFile();
@@ -48,12 +56,19 @@ public class NumberOfLines {
 				flag=1;
 				}
 			}
-			if(flag==1)
+			if(flag==1){
 			System.out
 					.println("Total number of Lines from the Output File is \n"
 						  + finalOutputString);
-			if(flag==0)
+			Common_Utility.ReporterBaseTest.test.log(Status.PASS, "Step 1: Total number of Lines from the Output File is " +finalOutputString);
+			}
+			
+			else
+			{
+		
 				System.out.println("Please check if the Total Number of Lines entered is correct");
+				Common_Utility.ReporterBaseTest.test.log(Status.FAIL, "Step 1: Total number of Lines from the Output File is not matching with input file");
+			}
 		}
 
 		// Method to parse the text file and pull out the Matching pattern to
@@ -61,6 +76,7 @@ public class NumberOfLines {
 
 		public static List<String> getTotalNumberOfLines() throws IOException {
 			try {
+				
 				String line = null;
 				String newLine = "";
 				@SuppressWarnings("resource")
